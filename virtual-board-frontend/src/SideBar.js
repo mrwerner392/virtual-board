4a8670489a06b7a21ad786a9908a010a10599a9a
kmarks2013 <kmarks2013@gmail.com>
2019-10-01 10:58:49 -0400

    Side bar js organization

--- virtual-board-frontend/src/SideBar.js
+++ virtual-board-frontend/src/SideBar.js
@@ -1,14 +1,14 @@
 class SideBar {
 
   constructor() {
-    this.sideBar = document.querySelector('#side-bar');
+    this.content = document.querySelector('#side-bar');
     this.usersList = [];
 
     UserAdapter.getAllUsers(usersData => {
       usersData.forEach(userObj => {
         let user = new User(userObj.id, userObj.name, userObj.age, userObj.bio);
         console.log(user);
-        this.sideBar.append(user.makePTag());
+        this.content.append(user.makePTag());
       });
     });
 
