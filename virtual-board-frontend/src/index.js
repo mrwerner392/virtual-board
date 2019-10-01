f6e3af82e754c741774ef628a3b699696df3df2c
mrwerner392 <mrwerner392@gmail.com>
2019-10-01 10:17:36 -0400

    users lists and profile info

--- virtual-board-frontend/src/index.js
+++ virtual-board-frontend/src/index.js
@@ -1,5 +1,3 @@
 const sideBar = document.querySelector('#side-bar');
 
-fetch("http://localhost:3000/users/2")
-.then(res => res.json())
-.then(userObj => sideBar.innerText = userObj.name)
+UserAdapter.getAllUsers(User.renderUsersList);
