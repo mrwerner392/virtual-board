e2fbe79e63d95bfdf7b285becafd8382bf6d3f8f
mrwerner392 <mrwerner392@gmail.com>
2019-10-04 17:10:15 -0400

    sidebar scroll and remove main container scroll

--- virtual-board-frontend/src/DoodleDot.js
+++ virtual-board-frontend/src/DoodleDot.js
@@ -1,7 +1,7 @@
 class DoodleDot {
 
-    static drawDot(context, x, y, color ,radius = 5) {
-      //add a color to the paramaters 
+    static drawDot(context, x, y, color ,radius = 2) {
+      //add a color to the paramaters
       context.fillStyle = color
       context.beginPath()
       context.arc(x , y , radius, 0 , 2 * Math.PI)
